5e7d4773e7c7fa4987d9341d7b344ddd2b04f67d
Alexander Ormandy <alexanderormandy@Alexanders-MBP.lan>
2019-02-12 15:39:35 +0000

    changes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let clock = document.getElementById("clock");
 let todayDate = document.getElementById("day");
+let greeting = document.getElementById("greeting");
 let day;
 let month;
 
@@ -16,8 +17,36 @@ let showClock = () => {
   } else {
   }
 
-  if (currentMonth == 1) {
+  if (currentMonth == 0) {
+    month = "January";
+  } else if (currentMonth == 1) {
     month = "February";
+  } else if (currentMonth == 3) {
+    month = "March";
+  } else if (currentMonth == 4) {
+    month = "April";
+  } else if (currentMonth == 5) {
+    month = "May";
+  } else if (currentMonth == 6) {
+    month = "June";
+  } else if (currentMonth == 7) {
+    month = "July";
+  } else if (currentMonth == 8) {
+    month = "August";
+  } else if (currentMonth == 9) {
+    month = "September";
+  } else if (currentMonth == 10) {
+    month = "October";
+  } else if (currentMonth == 11) {
+    month = "November";
+  } else if (currentMonth == 12) {
+    month = "December";
+  }
+
+  if (currentHour > 12) {
+    greeting.textContent = "Good Afternoon";
+  } else {
+    greeting.textContent = "Good Morning";
   }
 
   clock.textContent = `${currentHour} : ${currentMinute} ${currentSecond}`;
